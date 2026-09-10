768d93adf15c7d2a68a56a450c26378efff4d1fb
Michael Frost <mike0frost@gmail.com>
2026-09-10 21:39:07 +0200

    Add transpilePackages for Sanity Studio build on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['sanity', 'next-sanity', '@sanity/ui', '@sanity/icons', '@sanity/vision'],
   images: {
     remotePatterns: [
       {
