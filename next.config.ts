9fe86146b9b11f5c87198835e8eed79216f36c55
Michael Frost <mike0frost@gmail.com>
2026-09-10 21:47:44 +0200

    Remove embedded Studio (use delete-tv.sanity.studio instead); fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['sanity', 'next-sanity', '@sanity/ui', '@sanity/icons', '@sanity/vision'],
   images: {
     remotePatterns: [
       {
